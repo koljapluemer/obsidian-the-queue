02a0a6f6d81672902deadd5d5c803260737908cd
blackbrokkoli <kastomat@gmail.com>
2023-06-03 15:33:44 +0200

    create minimal modal and add in larger example with user input

--- main.ts
+++ main.ts
@@ -23,61 +23,23 @@ export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
-		await this.loadSettings();
-
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon(
 			"dice",
-			"Sample Plugin",
+			"Queue",
 			(evt: MouseEvent) => {
 				// Called when the user clicks the icon.
-				new SampleModal(this.app).open();
+				// new SampleModal(this.app).open();
+				new ExampleModal(this.app, (result) => {
+					new Notice(`Hello, ${result}!`);
+				}).open();
 			}
 		);
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass("my-plugin-ribbon-class");
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText("Status Bar Text");
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: "open-sample-modal-simple",
-			name: "Open sample modal (simple)",
-			callback: () => {
-				new SampleModal(this.app).open();
-			},
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, "click", (evt: MouseEvent) => {
-			console.log("click", evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(
-			window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
-		);
 	}
 
 	onunload() {}
-
-	async loadSettings() {
-		this.settings = Object.assign(
-			{},
-			DEFAULT_SETTINGS,
-			await this.loadData()
-		);
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
 }
 
 class SampleModal extends Modal {
@@ -96,11 +58,10 @@ class SampleModal extends Modal {
 			Math.floor(Math.random() * this.app.vault.getFiles().length)
 		];
 		// load the content of the random card
-		this.app.vault
-			.read(randomCard)
-			.then((content) => {
-				containerEl.createEl("p", { text: content });
-			});
+		this.app.vault.read(randomCard).then((content) => {
+			containerEl.createEl("br");
+			containerEl.createEl("p", { text: content });
+		});
 	}
 
 	onClose() {
@@ -109,33 +70,39 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+export class ExampleModal extends Modal {
+	result: string;
+	onSubmit: (result: string) => void;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
+	constructor(app: App, onSubmit: (result: string) => void) {
+		super(app);
+		this.onSubmit = onSubmit;
 	}
 
-	display(): void {
-		const { containerEl } = this;
+	onOpen() {
+		const { contentEl } = this;
 
-		containerEl.empty();
+		contentEl.createEl("h1", { text: "What's your name?" });
 
-		containerEl.createEl("h2", { text: "Settings for my awesome plugin." });
+		new Setting(contentEl).setName("Name").addText((text) =>
+			text.onChange((value) => {
+				this.result = value;
+			})
+		);
 
-		new Setting(containerEl)
-			.setName("Setting #1")
-			.setDesc("It's a secret")
-			.addText((text) =>
-				text
-					.setPlaceholder("Enter your secret")
-					.setValue(this.plugin.settings.mySetting)
-					.onChange(async (value) => {
-						console.log("Secret: " + value);
-						this.plugin.settings.mySetting = value;
-						await this.plugin.saveSettings();
-					})
-			);
+		new Setting(contentEl).addButton((btn) =>
+			btn
+				.setButtonText("Submit")
+				.setCta()
+				.onClick(() => {
+					this.close();
+					this.onSubmit(this.result);
+				})
+		);
+	}
+
+	onClose() {
+		let { contentEl } = this;
+		contentEl.empty();
 	}
 }
