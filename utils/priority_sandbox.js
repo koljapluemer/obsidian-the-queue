d32a7a13c0e8e74ba4eb76c46ad66de38556c94b
blackbrokkoli <kastomat@gmail.com>
2024-02-09 20:08:42 +0100

    add sandbox version of the priority calculation

---
+++ utils/priority_sandbox.js
@@ -0,0 +1,53 @@
+const cards =
+    [
+
+        {
+            name: "banana",
+            priority: 1
+        },
+        {
+            name: "apple",
+            priority: 20
+        },
+        {
+            name: "cherry",
+            priority: 300
+        },
+        {
+            name: "date",
+            priority: 4000
+        },
+        {
+            name: "elderberry",
+            priority: 50000
+        }
+    ];
+console.log(cards);
+
+function selectRandomCard(cards) {
+    // sort reversed priority
+    const sortedCards = cards.sort((a, b) => b.priority - a.priority);
+    const randomValue = Math.random();
+    // (e^x - 1) / (e - 1)
+    const pdfValueNormalized = (Math.exp(randomValue) - 1) / (Math.E - 1);
+    const amountOfCards = sortedCards.length;
+    const indexToSelect = Math.floor(pdfValueNormalized * amountOfCards);
+    return sortedCards[indexToSelect];
+}
+
+function main() {
+    // pick 1000 random cards, count how many times each card was picked
+    const cardCounts = {};
+    for (let i = 0; i < 10000; i++) {
+        const randomCard = selectRandomCard(cards);
+        if (cardCounts[randomCard.name]) {
+            cardCounts[randomCard.name]++;
+        } else {
+            cardCounts[randomCard.name] = 1;
+        }
+    }
+    const sortedCardCounts = Object.keys(cardCounts).sort((a, b) => cardCounts[a] - cardCounts[b]);
+    console.log('CARD COUNTS', sortedCardCounts.map(card => `${card}: ${cardCounts[card]}`));
+}
+
+main();
