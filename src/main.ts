a71717982e2159c2dac487e946f9d502654df0d8
blackbrokkoli <kastomat@gmail.com>
2024-09-04 12:17:35 +0300

    add semiworking custom function

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Plugin } from "obsidian";
+import { Plugin, MarkdownView } from "obsidian";
 
 import QueueSettingsTab from "./elements/QueueSettingsTab";
 import QueueModal from "./elements/QueueModal";
@@ -30,6 +30,7 @@ export default class TheQueue extends Plugin {
 	async onload() {
 		// This creates an icon in the left ribbon.
 		this.addRibbonIcon("sticky-note", "Start your queue", (evt: MouseEvent) => {
+			console.log('hi, opening...')
 			/** Here, the modal where the action happens is opened; see class definition */
 			new QueueModal(this.app, this.settings).open();
 		});
@@ -48,6 +49,13 @@ export default class TheQueue extends Plugin {
 		);
 
 		QueueLog.loadFromLocalStorage();
+
+		this.addRibbonIcon("clock", "Start queue in view", (evt: MouseEvent) => {
+			/** Here, the modal where the action happens is opened; see class definition */
+			console.log('opening...')
+			this.addButtonBarToActiveView();
+			// new QueueModal(this.app, this.settings).open();
+		});
 	}
 
 	onunload() {
@@ -64,4 +72,40 @@ export default class TheQueue extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+
+
+    addButtonBarToActiveView() {
+		console.log('adding button bar...')
+        const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+        if (activeView) {
+            const container = activeView.containerEl.querySelector('.view-content');
+            if (container) {
+                // Create the button bar
+                const buttonBar = document.createElement('div');
+                buttonBar.style.display = 'flex';
+                buttonBar.style.justifyContent = 'flex-end';
+                buttonBar.style.marginBottom = '10px';
+                
+                // Add buttons to the bar
+                const button1 = document.createElement('button');
+                button1.textContent = 'Button 1';
+                button1.onclick = () => {
+                    new Notice('Button 1 clicked!');
+                };
+
+                const button2 = document.createElement('button');
+                button2.textContent = 'Button 2';
+                button2.onclick = () => {
+                    new Notice('Button 2 clicked!');
+                };
+
+                buttonBar.appendChild(button1);
+                buttonBar.appendChild(button2);
+
+                // Insert the button bar into the view
+                container.prepend(buttonBar);
+            }
+        }
+    }
 }
