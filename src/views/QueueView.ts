c12eca2acf3d46c47759a195d2332f174618367c
blackbrokkoli <kastomat@gmail.com>
2024-09-14 12:29:37 +0200

    add some neat buttons as a row

--- src/views/QueueView.ts
+++ src/views/QueueView.ts
@@ -1,50 +1,66 @@
-import { MarkdownView, WorkspaceLeaf } from 'obsidian';
-import { QueueNote } from '../models/QueueNote';
-import { QueueManager } from '../managers/QueueManager'; // Import the manager to handle file loading
+import { Component, MarkdownView, WorkspaceLeaf } from "obsidian";
+import { QueueNote } from "../models/QueueNote";
+import { QueueManager } from "../managers/QueueManager"; // Import the manager to handle file loading
 
 import log from "../logger"; // Import loglevel logger
 
-
 export class QueueView extends MarkdownView {
-  note: QueueNote;
-  private queueManager: QueueManager;
+	note: QueueNote;
+	private queueManager: QueueManager;
 
-  constructor(leaf: WorkspaceLeaf, note: QueueNote, queueManager: QueueManager) {
-    super(leaf);
-    this.note = note;
-    this.queueManager = queueManager;
-    this.addButtonsBasedOnNoteType();
-  }
+	constructor(
+		leaf: WorkspaceLeaf,
+		note: QueueNote,
+		queueManager: QueueManager
+	) {
+		super(leaf);
+		this.note = note;
+		this.queueManager = queueManager;
+		this.addButtonsBasedOnNoteType();
 
-  // Add buttons based on the note type and strategy
-  addButtonsBasedOnNoteType() {
-    log.info("Adding buttons based on note type");
-    // find by class: .view-content
-    // const parentEl = this.containerEl; // Use the container element for button placement
-    // find by class .app-container
-    const parentEl = document.querySelector('.app-container');
-    if (!parentEl) {
-      log.error("Parent element not found");
-      return;
-    }
+        // get html parent of parentcontainer
+        const parentContainer = this.containerEl.parentElement;
 
-    // Clear any existing buttons before adding new ones
-    const existingButtons = parentEl.querySelectorAll('.queue-note-button');
-    existingButtons.forEach(button => button.remove());
+        // add an HTML element h1 "welcome to your view" on top
+        const container = this.containerEl.children[1];
+        const h1 = document.createElement("h1");
+        h1.innerText = "Welcome to your view";
+        parentContainer!.createEl("h1", { text: "Welcome to your view!" });
+        console.log("parentContainer", parentContainer);
 
-    // Get the buttons from the note's strategy
-    const buttons = this.note.getButtons();
-    log.info("Buttons:", buttons);
+        console.log("container", container);
+	}
 
-    // Add each button to the editor container
-    buttons.forEach(button => {
-        parentEl.createEl('button', {text: 'Click Me'});
-    //   button.classList.add('queue-note-button'); // Add class for easy styling
-    //   button.addEventListener('click', async () => {
-    //     // When a button is clicked, load a new random file in this QueueView
-    //     await this.queueManager.loadNewRandomFileInQueueView(this);
-    //   });
-    //   log.info("Button added to editor container:", button);
-    });
-  }
+	// Add buttons based on the note type and strategy
+	addButtonsBasedOnNoteType() {
+
+
+		log.info("Adding buttons based on note type");
+		// find by class: .view-content
+		// const parentEl = this.containerEl; // Use the container element for button placement
+		// find by class .app-container
+		const parentEl = document.querySelector(".mod-active");
+		if (!parentEl) {
+			log.error("Parent element not found");
+			return;
+		}
+
+
+		const buttonWrapper = document.createElement("div");
+		buttonWrapper.className = "queue-note-buttons";
+		parentEl.appendChild(buttonWrapper);
+
+		// Clear any existing buttons before adding new ones
+		const existingButtons = parentEl.querySelectorAll(".queue-note-button");
+		existingButtons.forEach((button) => button.remove());
+
+		// Get the buttons from the note's strategy
+		const buttons = this.note.getButtons();
+		log.info("Buttons:", buttons);
+
+		// Add each button to the editor container
+		buttons.forEach((button) => {
+			buttonWrapper.appendChild(button);
+		});
+	}
 }
