f5a35ba8de4bf901305368588d11b7fe273e8598
blackbrokkoli <kastomat@gmail.com>
2024-12-10 14:49:07 +0100

    add red tests for habit

--- src/tests/readWriteNotes.spec.ts
+++ src/tests/readWriteNotes.spec.ts
@@ -1,4 +1,5 @@
 import { getNoteFromString } from "../queueNoteUtils"
+import { mdHabitFists, noteHabitFists, yamlHabitFists } from "./data/habitShakeOutFists"
 import { mdLearnEgBenefitFromOg, noteLearnEgBenefitFromOg, yamlLearnEgBenefitFromOg } from "./data/learnEgBenefitFrom"
 
 test(`legacy learn note parses correctly`, () => {
@@ -11,4 +12,18 @@ test(`learn note parses correctly`, () => {
     expect(
         getNoteFromString(yamlLearnEgBenefitFromOg)
     ).toMatchObject(noteLearnEgBenefitFromOg)
+})
+
+
+
+test(`legacy habit note parses correctly`, () => {
+    expect(
+        getNoteFromString(mdHabitFists)
+    ).toMatchObject(noteHabitFists)
+})
+
+test(`habit note parses correctly`, () => {
+    expect(
+        getNoteFromString(yamlHabitFists)
+    ).toMatchObject(noteHabitFists)
 })
